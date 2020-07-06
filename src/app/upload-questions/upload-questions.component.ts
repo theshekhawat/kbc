b14e9e2875bfbb4859928b67b6efb07c5f7cdf18
Bhawani Shekhawat <bhawani.shekhawat@epicor.com>
2020-07-06 14:00:31 +0530

    updated upload questions

--- src/app/upload-questions/upload-questions.component.ts
+++ src/app/upload-questions/upload-questions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IQuizQuestionsSet } from '../model/IQuizQuestionsSet';
 
 @Component({
   selector: 'app-upload-questions',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadQuestionsComponent implements OnInit {
 
-  constructor() { }
+  private questionSetArray:Array<IQuizQuestionsSet>;
+  public questionSet: IQuizQuestionsSet;
+  public optionsArray: Array<string> = ['optionOne', 'optionTwo', 'optionThree', 'optionFour'];
+  constructor() {
+    this.questionSet = {
+      audiencePoll: null,
+      question: '',
+      optionOne: '',
+      optionTwo: '',
+      optionThree: '',
+      optionFour: '',
+      rightAnswer: this.optionsArray[0],
+      information: ''
+    }
+  }
 
   ngOnInit(): void {
   }
 
-}
+  submitted = false;
+
+  onSubmit() { this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  get diagnostic() { return JSON.stringify(this.questionSetArray); }
+
+  }
