e6bdda31253c055eb849592256bce9c14777fa50
Bhawani Shekhawat <bhawani.shekhawat@epicor.com>
2022-02-21 19:52:34 +0530

    Updated to Angular 11

--- src/app/game-components/fastest-finger-first/fastest-finger-first.component.spec.ts
+++ src/app/game-components/fastest-finger-first/fastest-finger-first.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { FastestFingerFirstComponent } from './fastest-finger-first.component';
 
@@ -6,7 +6,7 @@ describe('FastestFingerFirstComponent', () => {
   let component: FastestFingerFirstComponent;
   let fixture: ComponentFixture<FastestFingerFirstComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ FastestFingerFirstComponent ]
     })
